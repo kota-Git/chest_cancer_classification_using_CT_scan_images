ef3f2ddcf0ba110e3fcb3b63586e8841c6c25837
Laxman <lakshmanteja9@gmail.com>
2025-02-04 11:44:20 +0530

    model_trainer.ipynb_added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -9,6 +9,7 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
+
 @dataclass(frozen=True)
 class PrepareBaseModelConfig:
     root_dir: Path
@@ -19,4 +20,5 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+
 
